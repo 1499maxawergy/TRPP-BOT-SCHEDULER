eb4a6a42075063a6222844570e4e8ee711729769
Maxim Milko <maxawergy@yandex.ru>
2022-03-19 20:39:10 +0300

    Added: - API-token - Simple echo-bot in main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Begin")
+import telebot
+
+# Создаем экземпляр бота
+bot = telebot.TeleBot('5240599342:AAHOmtjA9_fmctqHapE66UeFfqcycJNQLlw')
+
+
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+
+
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+
+
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)

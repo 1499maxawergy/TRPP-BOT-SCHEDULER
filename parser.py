30c5ba7146014c47c4c8fa0a4858e69032b496bb
Maxim Milko <maxawergy@yandex.ru>
2022-03-21 20:08:46 +0300

    Added: - in parser.py added get_xlsx for find xlsx file by group name - started print_week

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ import openpyxl
 group_cell = dict()
 
 
+# init - просматривает все xlsx файлы и создает словарь с нахождением расписания этих групп
 def init():
     for number_of_xlxs in range(1, 4):
         sheet = openpyxl.load_workbook("iit" + str(number_of_xlxs) + ".xlsx").active
@@ -11,4 +12,24 @@ def init():
                 group_cell[sheet[2][column].value] = column
 
 
-print(group_cell)
+# get_xlsx - по имени группы выводит название файла, в котором лежит расписание с этой группой
+def get_xlsx(group_name):
+    if group_name[-2:] == "21":
+        return "iit1.xlsx"
+    elif group_name[-2:] == "20":
+        return "iit2.xlsx"
+    elif group_name[-2:] == "19":
+        return "iit3.xlsx"
+    elif group_name[-2:] == "18":
+        return "iit4.xlsx"
+    else:
+        return None
+
+
+def print_week(group_name):
+    if group_cell.get(group_name) is not None:
+        excel_filename = get_xlsx(group_name)
+        sheet = openpyxl.load_workbook(excel_filename).active
+        # todo
+    else:
+        return "Ваша группа не найдена."

fc9703e4d4bb4200419794d195af0afae3433cf7
Maxim Milko <maxawergy@yandex.ru>
2022-03-21 19:19:25 +0300

    Changed - parser.py initiate all .xlxs files

--- parser.py
+++ parser.py
@@ -1,11 +1,11 @@
 import openpyxl
 
-sheet = openpyxl.load_workbook("iit2.xlsx").active
-
 group_cell = dict()
 
-for column in range(5, sheet.max_column, 5):
-    if sheet[2][column].value != "День недели":
-        group_cell[sheet[2][column].value] = column
+for number_of_xlxs in range(1, 4):
+    sheet = openpyxl.load_workbook("iit" + str(number_of_xlxs) + ".xlsx").active
+    for column in range(5, sheet.max_column, 5):
+        if sheet[2][column].value != "День недели":
+            group_cell[sheet[2][column].value] = column
 
 print(group_cell)

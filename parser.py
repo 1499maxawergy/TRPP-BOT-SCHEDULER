6057087902efc94231e83c13cde688fe690228bd
Maxim Milko <maxawergy@yandex.ru>
2022-03-21 19:41:24 +0300

    Changed - parser.py added function init() for initiate all .xlsx files

--- parser.py
+++ parser.py
@@ -2,10 +2,13 @@ import openpyxl
 
 group_cell = dict()
 
-for number_of_xlxs in range(1, 4):
-    sheet = openpyxl.load_workbook("iit" + str(number_of_xlxs) + ".xlsx").active
-    for column in range(5, sheet.max_column, 5):
-        if sheet[2][column].value != "День недели":
-            group_cell[sheet[2][column].value] = column
+
+def init():
+    for number_of_xlxs in range(1, 4):
+        sheet = openpyxl.load_workbook("iit" + str(number_of_xlxs) + ".xlsx").active
+        for column in range(5, sheet.max_column, 5):
+            if sheet[2][column].value != "День недели":
+                group_cell[sheet[2][column].value] = column
+
 
 print(group_cell)

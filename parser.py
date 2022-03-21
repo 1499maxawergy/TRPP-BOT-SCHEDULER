13611a954367bcf275043c92462229b6e596397f
EZzic <max.zvyagincev@mail.ru>
2022-03-21 20:46:01 +0300

    Changed: - parser.py add week divide

--- parser.py
+++ parser.py
@@ -26,10 +26,26 @@ def get_xlsx(group_name):
         return None
 
 
-def print_week(group_name):
+def print_week(group_name, week):
     if group_cell.get(group_name) is not None:
         excel_filename = get_xlsx(group_name)
         sheet = openpyxl.load_workbook(excel_filename).active
-        # todo
+
+        for day in range(4, 64, 12):
+            print(sheet[day][0].value)
+            for para in range(day + week, day + 12, 2):
+                if sheet[para][group_cell[group_name]].value is not None:
+                    if sheet[para][group_cell[group_name] - 3].value is not None:
+                        print(str(sheet[para][group_cell[group_name] - 3].value), end="\t")
+                        print(str(sheet[para][group_cell[group_name] - 2].value), end="\t")
+                    else:
+                        print(str(sheet[para - 1][group_cell[group_name] - 3].value), end="\t")
+                        print(str(sheet[para - 1][group_cell[group_name] - 2].value), end="\t")
+
+                    print(str(sheet[para][group_cell[group_name]].value))
+            print()
     else:
         return "Ваша группа не найдена."
+
+init()
+print_week("ИАБО-01-20", 1)
